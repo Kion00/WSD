a07a1a2cc0a90a0d9f9778c5cc95fd5631fc7cbe
Kian Southgate <drumingkid@gmail.com>
2016-05-25 16:36:14 +1000

    Tried to create rest service but failed

--- src/uts/wsd/rest/PollService.java
+++ src/uts/wsd/rest/PollService.java
@@ -1,5 +1,44 @@
 package uts.wsd.rest;
+import java.io.IOException;
 
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.xml.bind.JAXBException;
+
+import uts.wsd.*;
+import uts.wsd.PollApplication;
+import uts.wsd.Polls;
+import uts.wsd.Users;
+@Path("/poll")
 public class PollService {
-	
+	@Context
+	 private ServletContext application;
+	 
+	 private PollApplication getpollApp() throws JAXBException, IOException {
+	  // The web server can handle requests from different clients in parallel.
+	  // These are called "threads".
+	  //
+	  // We do NOT want other threads to manipulate the application object at the same
+	  // time that we are manipulating it, otherwise bad things could happen.
+	  //
+	  // The "synchronized" keyword is used to lock the application object while
+	  // we're manpulating it.
+	  synchronized (application) {
+	   PollApplication pollApp = (PollApplication)application.getAttribute("pollApp");
+	   if (pollApp == null) {
+	    pollApp = new PollApplication();
+	    pollApp.setFilePath(application.getRealPath("WEB-INF/polls.xml"));
+	    application.setAttribute("pollApp", pollApp);
+	   }
+	   return pollApp;
+	  }
+	 }
+	 @Path("polls")
+	 @GET
+	 @Produces(MediaType.APPLICATION_XML)
+	 public Polls getPolls() throws JAXBException, IOException {
+		 
+		 return getpollApp().getPolls();
+	 }
 }
